2c4becaa71fa192958285101d3f866040e782354
Jeremy Stephens <viking415@gmail.com>
2010-04-05 21:24:45 -0500

    Generated home controller and comments scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Comments::Application.routes.draw do |map|
+  resources :comments
+
+  get "home/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +52,7 @@ Comments::Application.routes.draw do |map|
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "home#index"
 
   # See how all your routes lay out with "rake routes"
 
